2c65648a60842a367ab2598ed0b6a932973aa810
Mohamed8706 <bboge102@gmail.com>
2024-12-09 00:53:11 +0200

    progress bar for images and submiting dummy data

--- src/Pages/Dashboard/AddProduct.jsx
+++ src/Pages/Dashboard/AddProduct.jsx
@@ -13,6 +13,14 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 export default function AddProduct() {
 // States
+const [id, setId] = useState("");
+const [images, setImages] = useState([]);
+const [cat, setCat] = useState([]);
+// Loading
+const [loading, setLoading] = useState(false);
+const [sent, setSent] = useState(false);
+const [err, setErr] = useState("");
+// Forms
 const  [form, setForm] = useState({
     category: 'Select Category',
     title: '',
@@ -22,20 +30,16 @@ const  [form, setForm] = useState({
     About: ''
 
 })
+const dummyData = {
+    category: null,
+    title: 'dummy',
+    description: 'dummy',
+    price: '220',
+    discount: '0',
+    About: 'About'
 
-const [images, setImages] = useState([]);
-console.log(images)
+}
 
-const [cat, setCat] = useState([]);
-
-
-
-// Loading
-const [loading, setLoading] = useState(false);
-
-
-// Err
-const [err, setErr] = useState("");
 
 // Ref
 const focus = useRef("");
@@ -72,28 +76,25 @@ const token = cookie.get("e-commerce");
 
 // handle form change
 function handleChange(e) {
-    setForm({...form, [e.target.name] : e.target.value})
+    setForm({...form, [e.target.name] : e.target.value});
+    setSent(1)
+    if (sent !== 1){
+        handleFormSubmit();
+
+    }
+
 }
 
 
 
-// handle form submit
-async function handleSubmit(e) {
+// handle form edit
+async function handleEdit(e) {
     e.preventDefault();
     setLoading(true)
     try {
-        const data = new FormData();
-        data.append("category", form.category);
-        data.append("title", form.title);
-        data.append("description", form.description);
-        data.append("price", form.price);
-        data.append("discount", form.discount);
-        data.append("About", form.About);
-        for (let index = 0; index < images.length; index++) {
-            data.append("images[]", images[index])
-            
-        }
-        const res = await axios.post(`${baseUrl}/${Product}/${ADD}`, data, {
+    
+
+        const res = await axios.post(`${baseUrl}/${Product}/edit/${id}`, form, {
         headers: {
             Authorization: "Bearer " + token,
         },
@@ -115,10 +116,25 @@ async function handleSubmit(e) {
     }
 }
 
+// Handle initial Form submit
+async function handleFormSubmit() {
+    try {
+        const res = await axios.post(`${baseUrl}/${Product}/${ADD}`, dummyData, {
+            headers: {
+                Authorization: "Bearer " + token,
+            }
+        }).then((data) => setId(data.data.id))
+    } catch (error) {
+        console.log(error)
+    }
+}
+
 // Maping 
 const categories = cat.map((cat, ind) => <option key={ind} value={cat.id}>{cat.title}</option>);
 
-const imagesShow = images.map((img, key) => <div className="flex flex-row border w-100 gap-2 p-2">
+const imagesShow = images.map((img, key) => 
+    <div key={key} className="w-100 border p-2">
+    <div className="flex flex-row gap-2 ">
     <img src={URL.createObjectURL(img)} alt="product" className="w-[80px]"/>
     <div className="flex justify-center flex-col">
     <p>{img.name}</p>
@@ -127,13 +143,20 @@ const imagesShow = images.map((img, key) => <div className="flex flex-row border
             : (img.size / (1024 * 1024)).toFixed(2) + "MB"} 
     </p>
     </div>    
-    </div>)
+    </div>
+    <div className="custom-progress">
+        <span percent={'50%'} className="inner-progress">
+
+        </span>
+    </div>
+    </div>
+    )
 
 return (
     <>
         {loading && <LoadingSubmit />}
             <div className="row bg-gray-100" style={{margin:"2px"}}>
-                <Form onSubmit={handleSubmit} className="h-[95%] m-1">
+                <Form onSubmit={handleEdit} className="h-[95%] m-1">
                     <div className="h-100 bg-white p-5 rounded-xl shadow-2xl">
 
 
@@ -143,7 +166,10 @@ return (
                                 className="w-100"
                                 name="category"
                                 value={form.category}
-                                onChange={(e) => handleChange(e)}
+                                onChange={(e) => {
+                                    handleChange(e)
+                                    
+                                }}
                                 ref={focus}
                                 
                             >
@@ -165,7 +191,7 @@ return (
                                 name="title"
                                 value={form.title}
                                 onChange={(e) => handleChange(e)}
-                                required
+                                disabled={!sent}
                             
                             />
                             <Form.Label>Title</Form.Label>
@@ -183,7 +209,7 @@ return (
                                 name="description"
                                 value={form.description}
                                 onChange={(e) => handleChange(e)}
-                                required
+                                disabled={!sent}
                                 
                             />
                             <Form.Label>Description</Form.Label>
@@ -202,7 +228,7 @@ return (
                                 name="price"
                                 value={form.price}
                                 onChange={(e) => handleChange(e)}
-                                required
+                                disabled={!sent}
                                 
                             />
                             <Form.Label>Price</Form.Label>
@@ -221,7 +247,7 @@ return (
                                 name="discount"
                                 value={form.discount}
                                 onChange={(e) => handleChange(e)}
-                                required
+                                disabled={!sent}
                                 
                             />
                             <Form.Label>Discount</Form.Label>
@@ -239,7 +265,7 @@ return (
                                 name="About"
                                 value={form.About}
                                 onChange={(e) => handleChange(e)}
-                                required
+                                disabled={!sent}
                                 
                             />
                             <Form.Label>About</Form.Label>
@@ -263,9 +289,11 @@ return (
                         </Form.Group>
                         <div onClick={uploadImage}
                         className="flex items-center justify-center py-3 w-100 flex-col rounded 
-                        border-dashed border-[2px] gap-2 border-[#0086fe] cursor-pointer">
-                        <img src={require("../../Assets/upload.png")} alt="upload" className="w-[100px]"/>
-                        <p className="font-bold text-[#0086fe]">Upload Images</p>
+                         gap-2 cursor-pointer"
+                        style={{border: !sent ? "2px dashed gray" : "2px dashed #0086fe"}}
+                        >
+                        <img style={{filter: !sent && "grayscale(1)"}} src={require("../../Assets/upload.png")} alt="upload" className="w-[100px]"/>
+                        <p style={{color: !sent ? "gray" : "#0086fe"}} className="font-bold">Upload Images</p>
                         </div>
                     <div className="flex flex-col p-4 items-start justify-center gap-4">
                             {imagesShow} 
